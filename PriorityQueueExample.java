547c03d0422f91a71cedca9368d4f1649826c6ae
Suranjan kumar <ksuranjan951@gmail.com>
2024-08-17 20:31:55 +0530

    PriorityQueueExample

---
+++ PriorityQueueExample.java
@@ -0,0 +1,13 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        pq.add(100);
+        pq.add(50);
+        pq.add(150);
+        pq.add(25);
+        pq.add(25);
+        System.out.println(pq);
+    }
+}
